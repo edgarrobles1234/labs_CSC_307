e002a1f44396e0cf36a3429d71440c4c68e750ae
edgarrobles1234 <edgarrobles1234@yahoo.com>
2025-04-23 10:40:58 -0700

    merged new main

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -3,73 +3,6 @@ import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
-<<<<<<< HEAD
-  function MyApp() {
-    const [characters, setCharacters] = useState([]);
-  
-    function removeOneCharacter(index) {
-      /*
-        const updated = characters.filter((character, i) => {
-          return i !== index;
-        });
-        setCharacters(updated);*/
-        const promise = fetch("Http://localhost:8000/users/:"+index, {
-          method: "DELETE",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(person),
-        });
-      
-        return promise;
-      }
-
-      function fetchUsers() {
-        const promise = fetch("http://localhost:8000/users");
-        return promise;
-      }
-
-      function postUser(person) {
-        const promise = fetch("Http://localhost:8000/users", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(person),
-        });
-      
-        return promise;
-      }
-
-      function updateList(person) {
-        postUser(person)
-          .then((response) => {if(response.status === 201){setCharacters([...characters, person])}})
-          .then((response) => response.JSON())
-          .catch((error) => {
-            console.log(error);
-          });
-      }
-
-      useEffect(() => {
-        fetchUsers()
-          .then((res) => res.json())
-          .then((json) => setCharacters(json["users_list"]))
-          .catch((error) => {
-            console.log(error);
-          });
-      }, []);
-    
-      return (
-        <div className="container">
-          <Table
-            characterData={characters}
-            removeCharacter={removeOneCharacter}
-          />
-          <Form handleSubmit={updateList} />
-        </div>
-      );
-      
-=======
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
@@ -82,7 +15,6 @@ function MyApp() {
       body: JSON.stringify(person)
     });
     return promise;
->>>>>>> new_main
   }
 
   function removeOneCharacter(index) {
